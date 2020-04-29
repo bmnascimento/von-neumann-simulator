9856e036b1693f68c7fb84671bdfe1239f575ff0
Bernardo <b@bmnascimento.com>
2020-04-29 15:33:23 -0300

    added gh-pages to allow deploying to github

---
+++ docs/build/precache-manifest.b6cd4d5dd8e2261cd7ac528fa026b1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b254f85b6b7e2cc511a72ba7e504e3df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f5634518b7ccb8a275",
+    "url": "/static/css/main.e27f258b.chunk.css"
+  },
+  {
+    "revision": "96a89650e41e24c92922",
+    "url": "/static/js/2.d0c02747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0c02747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f5634518b7ccb8a275",
+    "url": "/static/js/main.8915140b.chunk.js"
+  },
+  {
+    "revision": "dcac98617df17f5625f8",
+    "url": "/static/js/runtime-main.6fdc1a82.js"
+  }
+]);
