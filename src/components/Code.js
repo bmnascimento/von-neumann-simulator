253faf178e9b2d4c6525c2883cf8390e0dbff5bd
Bernardo <b@bmnascimento.com>
2020-04-29 00:03:12 -0300

    inicio

---
+++ src/components/Code.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const Code = () => {
+  const codeStyle = {
+    flex: 1, 
+    display: 'flex',
+    flexDirection: 'column',
+  }
+
+  const editorStyle = {
+    display: 'block',
+    resize: 'none',
+    overflowX: 'auto',
+    width: '95%',
+    flex: 1,
+    margin: 'auto',
+    textAlign: 'left',
+    padding: 5,
+    backgroundColor: '#1e1e1e',
+    color: '#d4d4d4',
+    borderStyle: 'none',
+    fontSize: '1.3em',
+  }
+
+  const buttonStyle = {
+    margin: 10,
+  }
+
+  const [ code, setCode ] = useState('')
+
+  const handleSimulate = () => {
+    console.log(code)
+  }
+
+  return(
+    <div style={codeStyle}>
+      <textarea value={code} onChange={event => setCode(event.target.value)} style={editorStyle} wrap="off"/>
+      <button onClick={handleSimulate} style={buttonStyle}>Simulate</button>
+    </div>
+  )
+}
+
+export default Code
---
+++ src/components/Memory.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Memory = () => {
+  const memoryStyle = {
+    flex: 1, 
+  }
+
+  return(
+    <div style={memoryStyle}>
+      3
+    </div>
+  )
+}
+
+export default Memory
